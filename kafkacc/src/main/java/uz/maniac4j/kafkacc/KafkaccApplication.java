659915dd2ce3d7cfdfb703a3185360acd2c380bc
Mahmud <mahmud.salomov.salomov@gmail.com>
2022-03-25 09:52:16 +0500

    Initial commit

---
+++ kafkacc/src/main/java/uz/maniac4j/kafkacc/KafkaccApplication.java
@@ -0,0 +1,13 @@
+package uz.maniac4j.kafkacc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaccApplication.class, args);
+	}
+
+}
---
+++ kafkacc/src/test/java/uz/maniac4j/kafkacc/KafkaccApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.maniac4j.kafkacc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
